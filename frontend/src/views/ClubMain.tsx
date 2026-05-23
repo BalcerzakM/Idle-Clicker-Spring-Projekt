c7995a132e3d3f7f24e50589f8c6d4248373c33b
BalcerzakM <mcj.balcerzak@gmail.com>
2026-05-23 12:07:05 +0200

    barman na hover

--- frontend/src/views/ClubMain.tsx
+++ frontend/src/views/ClubMain.tsx
@@ -1,11 +1,21 @@
+import barmanHover from "../assets/scenes/hover/barman_hover.png";
+
 function ClubMain() {
-    
-    return (
-        <div className="clubMain">
-            <div className="clubMain-barman"></div>
-            <p className="clubMain-barmanTextBox">Barman</p>
-        </div>
-    )
+	return (
+		<div className="clubMain">
+			<div className="clubMain-barman">
+				<img
+					src={barmanHover}
+					alt="barman_hover"
+					width={126}
+					height={141}
+					className="hover-image"
+				/>
+			</div>
+
+			<p className="clubMain-barmanTextBox">Barman</p>
+		</div>
+	);
 }
 
-export default ClubMain
+export default ClubMain;
