df3dcf8e6f50a76cef45d57b1bbda86980e22957
BalcerzakM <mcj.balcerzak@gmail.com>
2026-05-28 17:51:20 +0200

    pobieranie vatara, oraz jego pieniędzy z endpointa

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,16 +1,43 @@
 import StandardCurrencyImg from "../assets/other/currency_standard.png";
 import PremiumCurrencyImg from "../assets/other/currency_premium.png";
 import { useNavigate } from "react-router-dom";
+import { useState, useEffect, useCallback } from "react";
 
 function NavBar() {
 	const navigate = useNavigate();
+	const [character, setCharacter] = useState<CharacterShortInfo | null>(null);
+
+	interface CharacterShortInfo {
+		avatarPicture: string;
+		money: number;
+		cristals: number;
+	}
+
+	const fetchCharacterShortInfo = useCallback(async () => {
+		try {
+			const res = await fetch("http://localhost:8080/api/character/money");
+
+			if (!res.ok) {
+				throw new Error("Błąd pobierania danych postaci");
+			}
+
+			const heroData: CharacterShortInfo = await res.json();
+			setCharacter(heroData);
+		} catch (err: any) {
+			console.error(err);
+		}
+	}, []);
+
+	useEffect(() => {
+		fetchCharacterShortInfo();
+	}, [fetchCharacterShortInfo]);
 
 	return (
 		<div className="navBar">
 			<div className="navBar-character">
 				<button type="button" onClick={() => navigate("/player")}>
 					<img
-						src="/avatars/avatar1.png"
+						src={`/avatars/${character?.avatarPicture}`}
 						alt="Player avatar"
 						className="playerAvatar"
 					/>
@@ -21,13 +48,13 @@ function NavBar() {
 						alt="Standard currency"
 						className="currencyImg"
 					/>
-					<p>1000</p>
+					<p>{`${character?.money}`}</p>
 					<img
 						src={PremiumCurrencyImg}
 						alt="Premium currency"
 						className="currencyImg"
 					/>
-					<p>5</p>
+					<p>{`${character?.cristals}`}</p>
 				</div>
 			</div>
 			<div className="navBar-navigation">
