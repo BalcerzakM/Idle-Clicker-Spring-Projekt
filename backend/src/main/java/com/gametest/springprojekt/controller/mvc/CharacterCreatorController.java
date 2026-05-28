dcc4732ddf9bf4f278e2dcabf3b6d54ca5a65f66
pborysq <pborysq@gmail.com>
2026-05-28 02:53:34 +0200

    style stron thymeleaf, login + rejestracja prawie gotowe

--- backend/src/main/java/com/gametest/springprojekt/controller/mvc/CharacterCreatorController.java
+++ backend/src/main/java/com/gametest/springprojekt/controller/mvc/CharacterCreatorController.java
@@ -2,11 +2,14 @@ package com.gametest.springprojekt.controller.mvc;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/mvc/character-creator")
 public class CharacterCreatorController {
+
+    @GetMapping
     public String characterCreator() {
         return "character-creator";
     }
