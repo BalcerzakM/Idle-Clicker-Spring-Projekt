773bac146112eee67421baec93becfa35b6f1cd1
janek <91958@student.pb.edu.pl>
2026-06-07 20:52:25 +0200

    hovery palarni i warty

--- frontend/src/views/ClubMain.tsx
+++ frontend/src/views/ClubMain.tsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import barmanHover from "../assets/scenes/hover/barman_hover.png";
 import szatniarzHover from "../assets/scenes/hover/szatniarz_hover.png";
+import outsideHover from "../assets/scenes/hover/outside_hover.png";
 
 function ClubMain() {
 	const navigate = useNavigate();
@@ -16,7 +17,6 @@ function ClubMain() {
 					onClick={() => navigate("/barman")}
 				/>
 			</div>
-
 			<p className="clubMain-barmanTextBox">Barman</p>
 
 			<div className="clubMain-szatniarz">
@@ -30,8 +30,20 @@ function ClubMain() {
 
 				/>
 			</div>
-
 			<p className="clubMain-szatniarzTextBox">Szatniarz</p>
+
+            <div className="clubMain-outside">
+                <img
+                    src={outsideHover}
+                    alt="outside_hover"
+                    width={725}
+                    height={204}
+                    className="hover-image"
+                    onClick={() => navigate("/outside")}
+
+                />
+            </div>
+            <p className="clubMain-outsideTextBox">Palarnia</p>
 		</div>
 	);
 }
