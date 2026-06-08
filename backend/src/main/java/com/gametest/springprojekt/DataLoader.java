397e4d2821e900e718160f7365e2ab9f95dd3630
pborysq <pborysq@gmail.com>
2026-06-09 00:49:31 +0200

    wyłączony dataLoader i zrobienie bazy danych na update zeby nie usuwało danych przy odpaleniu

--- backend/src/main/java/com/gametest/springprojekt/DataLoader.java
+++ backend/src/main/java/com/gametest/springprojekt/DataLoader.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-@Component
+//@Component
 public class DataLoader implements CommandLineRunner {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
