2c013de6e03428696cdef3f09fb6fdd905ece568
BalcerzakM <mcj.balcerzak@gmail.com>
2026-06-11 17:51:47 +0200

    raportowanie do admina v1

---
+++ backend/src/main/java/com/gametest/springprojekt/model/ReportEntity.java
@@ -0,0 +1,30 @@
+package com.gametest.springprojekt.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class ReportEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserEntity user;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String title;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String description;
+}
---
+++ backend/src/main/java/com/gametest/springprojekt/service/ReportService.java
@@ -0,0 +1,34 @@
+package com.gametest.springprojekt.service;
+
+import com.gametest.springprojekt.dto.ReportDto;
+import com.gametest.springprojekt.model.ReportEntity;
+import com.gametest.springprojekt.model.UserEntity;
+import com.gametest.springprojekt.repository.ReportRepository;
+import com.gametest.springprojekt.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ReportService {
+    private final ReportRepository reportRepository;
+    private final UserRepository userRepository;
+
+
+    public void addReportToDB(ReportDto report) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String username = auth.getName();
+
+        UserEntity user = userRepository.getByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Nie ma takiego użytkownika!"));
+
+        ReportEntity newReport = new ReportEntity();
+        newReport.setUser(user);
+        newReport.setTitle(report.getTitle());
+        newReport.setDescription(report.getDescription());
+        reportRepository.save(newReport);
+    }
+}
