139c7bb0b1eee2e39cb5da7c33968269b434a276
BalcerzakM <mcj.balcerzak@gmail.com>
2026-07-16 16:48:54 +0200

    oglądanie wojny gangów wersja beta

--- backend/src/main/java/com/gametest/springprojekt/dto/CharacterInBattleDto.java
+++ backend/src/main/java/com/gametest/springprojekt/dto/CharacterInBattleDto.java
@@ -2,9 +2,11 @@ package com.gametest.springprojekt.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class CharacterInBattleDto {
     private String characterName;
     private String imagePath;
